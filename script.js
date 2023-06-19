5ae719494f2e2c71c6b12b689d47481a7e5f2825
Amaan <amaan14999@gmail.com>
2023-06-19 19:18:01 +0530

    fix: proceed only when numcourses>0

--- script.js
+++ script.js
@@ -7,29 +7,31 @@ nextButton.addEventListener("click", nextB);
 
 function nextB() {
   const n = parseInt(numCoursesInput.value);
-  let coursesHtml = "";
-  for (let i = 1; i <= n; i++) {
-    coursesHtml += `<label for="credit${i}">Enter the credit points of course ${i}:</label>`;
-    coursesHtml += `<input type="number" id="credit${i}" step="0.01">`;
-    coursesHtml += `<label for="grade${i}">Enter the expected grade of course ${i}:</label>`;
-    coursesHtml += `<select id="grade${i}">`;
-    coursesHtml += `<option value="10">A</option>`;
-    coursesHtml += `<option value="9">A-</option>`;
-    coursesHtml += `<option value="8">B</option>`;
-    coursesHtml += `<option value="7">B-</option>`;
-    coursesHtml += `<option value="6">C</option>`;
-    coursesHtml += `<option value="5">C-</option>`;
-    coursesHtml += `<option value="4">D</option>`;
-    coursesHtml += `</select><br><br>`;
-  }
-  nextButton.innerText = "Calculate";
-  coursesDiv.innerHTML = coursesHtml;
-  nextButton.removeEventListener("click", nextB);
-  nextButton.addEventListener("click", calB);
-  if (n > 3) {
-    document.body.style.height = "100%";
-  } else {
-    document.body.style.height = "100vh";
+  if (n > 0) {
+    let coursesHtml = "";
+    for (let i = 1; i <= n; i++) {
+      coursesHtml += `<label for="credit${i}">Enter the credit points of course ${i}:</label>`;
+      coursesHtml += `<input type="number" id="credit${i}" step="0.01">`;
+      coursesHtml += `<label for="grade${i}">Enter the expected grade of course ${i}:</label>`;
+      coursesHtml += `<select id="grade${i}">`;
+      coursesHtml += `<option value="10">A</option>`;
+      coursesHtml += `<option value="9">A-</option>`;
+      coursesHtml += `<option value="8">B</option>`;
+      coursesHtml += `<option value="7">B-</option>`;
+      coursesHtml += `<option value="6">C</option>`;
+      coursesHtml += `<option value="5">C-</option>`;
+      coursesHtml += `<option value="4">D</option>`;
+      coursesHtml += `</select><br><br>`;
+    }
+    nextButton.innerText = "Calculate";
+    coursesDiv.innerHTML = coursesHtml;
+    nextButton.removeEventListener("click", nextB);
+    nextButton.addEventListener("click", calB);
+    if (n > 3) {
+      document.body.style.height = "100%";
+    } else {
+      document.body.style.height = "100vh";
+    }
   }
 }
 
